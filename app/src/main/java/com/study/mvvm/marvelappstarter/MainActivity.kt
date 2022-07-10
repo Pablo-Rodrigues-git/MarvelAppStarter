d9eec565547043219abd295700cffcbb61b9849f
Pablo Rodrigues <pablorodrigues@mobills.com.br>
2022-07-10 19:03:32 -0300

    feature: {nav_graph} instatiated navHostFragment to the MainActivity.kt

--- app/src/main/java/com/study/mvvm/marvelappstarter/MainActivity.kt
+++ app/src/main/java/com/study/mvvm/marvelappstarter/MainActivity.kt
@@ -2,15 +2,31 @@ package com.study.mvvm.marvelappstarter
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.study.mvvm.marvelappstarter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navHostFrament: NavHostFragment
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initView(binding)
+    }
+
+    private fun initView(binding: ActivityMainBinding) {
+        navHostFrament =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFrament.navController
+
+        binding.bottomNavigation.apply {
+            setupWithNavController(navController)
+            setOnNavigationItemReselectedListener { }
+        }
     }
 }
