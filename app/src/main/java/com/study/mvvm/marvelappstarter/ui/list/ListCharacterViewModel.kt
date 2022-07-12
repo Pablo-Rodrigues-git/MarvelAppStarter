a5b83c886e84487619f500a6e9ec3ff9825967e4
Pablo Rodrigues <pablorodrigues@mobills.com.br>
2022-07-12 20:40:18 -0300

    feature: {listOfCharacters} listing characters on the recyclerView from the api

--- app/src/main/java/com/study/mvvm/marvelappstarter/ui/list/ListCharacterViewModel.kt
+++ app/src/main/java/com/study/mvvm/marvelappstarter/ui/list/ListCharacterViewModel.kt
@@ -1,6 +1,54 @@
 package com.study.mvvm.marvelappstarter.ui.list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.study.mvvm.marvelappstarter.data.model.character.CharacterModelResponse
+import com.study.mvvm.marvelappstarter.repository.MarvelRepository
+import com.study.mvvm.marvelappstarter.ui.state.ResourceState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import java.io.IOException
+import javax.inject.Inject
 
-class ListCharacterViewModel: ViewModel() {
+@HiltViewModel
+class ListCharacterViewModel @Inject constructor(
+    private val repository: MarvelRepository,
+) : ViewModel() {
+
+    private val _list =
+        MutableStateFlow<ResourceState<CharacterModelResponse>>(ResourceState.Loading())
+    val list: StateFlow<ResourceState<CharacterModelResponse>> = _list
+
+    init {
+        fetch()
+    }
+
+    private fun fetch() = viewModelScope.launch {
+        safeFetch()
+    }
+
+    private suspend fun safeFetch() {
+        try {
+            val response = repository.list()
+            _list.value = handleResponse(response)
+        }catch (t: Throwable) {
+            when(t) {
+                is IOException -> _list.value = ResourceState.Error("Erro de conexão com a internet")
+                else -> _list.value = ResourceState.Error("Falha na conversão de dados")
+            }
+
+        }
+    }
+
+    private fun handleResponse(response: Response<CharacterModelResponse>): ResourceState<CharacterModelResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { values ->
+                return ResourceState.Sucess(values)
+            }
+        }
+        return ResourceState.Error(response.message())
+    }
 }
--- app/src/main/java/com/study/mvvm/marvelappstarter/util/Constants.kt
+++ app/src/main/java/com/study/mvvm/marvelappstarter/util/Constants.kt
@@ -2,7 +2,7 @@ package com.study.mvvm.marvelappstarter.util
 
 object Constants {
 
-    const val BASE_URL = "https://gateway.marvel.com/V1/public/"
+    const val BASE_URL = "https://gateway.marvel.com/v1/public/"
 
     const val PUBLIC_KEY = "9a08da9217cbad374e65148016b404c5"
     const val PRIVATE_KEY = "36b6763d327e566ffdeea55a6e01a2801496a131"
