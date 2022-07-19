11db27ea6d327e654a4f1c6ff43b46ac3a50276b
Pablo Rodrigues <pablorodrigues@mobills.com.br>
2022-07-19 19:42:59 -0300

    feature: {SearchCharacter} created and saved instance for rotation persistence

--- app/src/main/java/com/study/mvvm/marvelappstarter/util/Constants.kt
+++ app/src/main/java/com/study/mvvm/marvelappstarter/util/Constants.kt
@@ -7,6 +7,9 @@ object Constants {
     const val PUBLIC_KEY = "9a08da9217cbad374e65148016b404c5"
     const val PRIVATE_KEY = "36b6763d327e566ffdeea55a6e01a2801496a131"
 
+    const val LAST_SEARCH_QUERY = "last_search_query"
+    const val DEFAULT_QUERY = ""
+
     const val APIKEY = "apikey"
     const val HASH = "hash"
     const val TS = "ts"
