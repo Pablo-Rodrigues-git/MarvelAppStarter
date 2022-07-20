4dc1f1c788293550ada62ff422d3192c7f20f6bc
Pablo Rodrigues <pablorodrigues@mobills.com.br>
2022-07-19 21:09:07 -0300

    feature: {SwipeToDelete} creating swipeToDelete

--- app/src/main/java/com/study/mvvm/marvelappstarter/ui/favorite/FavoriteCharacterFragment.kt
+++ app/src/main/java/com/study/mvvm/marvelappstarter/ui/favorite/FavoriteCharacterFragment.kt
@@ -1,16 +1,90 @@
 package com.study.mvvm.marvelappstarter.ui.favorite
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.study.mvvm.marvelappstarter.databinding.FragmentFavoriteCharacterBinding
+import com.study.mvvm.marvelappstarter.ui.adapters.CharacterAdapter
 import com.study.mvvm.marvelappstarter.ui.base.BaseFragment
+import com.study.mvvm.marvelappstarter.ui.state.ResourceState
+import com.study.mvvm.marvelappstarter.util.hide
+import com.study.mvvm.marvelappstarter.util.show
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class FavoriteCharacterFragment :
     BaseFragment<FragmentFavoriteCharacterBinding, FavoriteCharacterFragmentViewModel>() {
     override val viewModel: FavoriteCharacterFragmentViewModel by viewModels()
+    private val characterAdapter by lazy { CharacterAdapter() }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        clickAdapter()
+        observer()
+    }
+
+    private fun observer() = lifecycleScope.launch {
+        viewModel.favorites.collect { resource ->
+            when (resource) {
+                is ResourceState.Sucess -> {
+                    resource.data?.let {
+                        binding.tvEmptyList.hide()
+                        characterAdapter.characters = it.toList()
+                    }
+
+                }
+                is ResourceState.Empty -> {
+                    binding.tvEmptyList.show()
+                }
+                else -> {}
+            }
+        }
+
+    }
+
+    private fun clickAdapter() {
+        characterAdapter.setOnClickListener { characterModel ->
+            val action = FavoriteCharacterFragmentDirections
+                .actionFavoriteCharacterFragmentToDetailsCharacterFragment(characterModel)
+            findNavController().navigate(action)
+        }
+    }
+
+    private fun setupRecyclerView() = with(binding) {
+        rvFavoriteCharacter.apply {
+            adapter = characterAdapter
+            layoutManager = LinearLayoutManager(context)
+
+        }
+    }
+
+    private fun itemTouchHelperCallBack(): ItemTouchHelper.SimpleCallback {
+        return object :
+            ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder,
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val character = characterAdapter
+            }
+
+        }
+    }
 
     override fun getViewBinding(
         inflater: LayoutInflater,
